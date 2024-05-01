7633ccde098025258aadf15e07c50bfa34d697da
mvsoom <mvsoom@gmail.com>
2024-05-01 13:40:12 +0000

    Improve

--- download_saved_model.py
+++ download_saved_model.py
@@ -5,17 +5,13 @@ import os
 # Define the Google Drive file URL (34 gb)
 file_url = 'https://drive.google.com/uc?id=1mq6fg69Ofm32-1HjEunoFtPg8ymAIcOp'
 
-# Define the directory to save the downloaded file and extracted contents
-extract_dir = './saved_model'
-os.makedirs(extract_dir, exist_ok=True)
-
 # Download the file
-tar_file_path = os.path.join(".", 'saved_model.tar')
+tar_file_path ="saved_model.tar"
 gdown.download(file_url, tar_file_path, quiet=False)
 
 # Untar the file
 with tarfile.open(tar_file_path, 'r') as tar_ref:
-    tar_ref.extractall(extract_dir)
+    tar_ref.extractall(".")
 
 # Remove the tar file
 os.remove(tar_file_path)
